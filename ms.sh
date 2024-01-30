89f08b5edbbc6f5cd5dfb206df62857ad35e7937
suminerProxy <97101851+suminerProxy@users.noreply.github.com>
2024-01-30 09:57:38 +0800

    Create ms.sh

---
+++ ms.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# 下载、解压、切换目录
+curl -o apphub-linux-amd64.tar.gz https://assets.coreservice.io/public/package/60/app-market-gaga-pro/1.0.4/app-market-gaga-pro-1_0_4.tar.gz \
+  && tar -zxf apphub-linux-amd64.tar.gz \
+  && rm -f apphub-linux-amd64.tar.gz \
+  && cd ./apphub-linux-amd64
+
+# 移除服务、安装服务
+sudo ./apphub service remove \
+  && sudo ./apphub service install
+
+# 启动服务
+sudo ./apphub service start
+
+# 查看状态
+./apphub status
+
+# 休眠3秒
+sleep 3
+
+# 配置token并重启
+sudo ./apps/gaganode/gaganode config set --token=hesbxducrynlhdxifee6336a5e279047 \
+  && ./apphub restart
